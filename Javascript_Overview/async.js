83a5c7eed8bf8852c3c7b311c658fa5c6da0b937
SomMathur <som.mathur@mail.utoronto.ca>
2018-12-30 00:29:06 -0500

    async concepts

---
+++ Javascript_Overview/async.js
@@ -0,0 +1,15 @@
+function printOne(){
+	console.log("one")
+}
+
+function printTwo(){
+	console.log("Two")
+}
+function printThree(){
+	console.log("Three")
+}
+
+//setTimeout(printOne)
+//setTimeout(printTwo,5000)
+setTimeout(printThree,0)
+setTimeout(printOne)
