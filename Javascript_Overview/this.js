7cb3caaad98e0b6ae4c4ce5e0fb8d9ff1d1df25a
SomMathur <som.mathur@mail.utoronto.ca>
2018-12-30 18:25:40 -0500

    Updated-call,bind,apply

--- Javascript_Overview/this.js
+++ Javascript_Overview/this.js
@@ -11,4 +11,21 @@ person.greet()//method called, this is attached to object
 //But
 const greet1=person.greet
 greet1()//since this is attached to the object, it will not refer to name
-//undefined
+//undefined, the this will refer to global object
+
+const friend={
+	name:"Sumant"
+}
+
+friend.greet=person.greet//now this is bound to new object
+
+
+//1.Setting this explicitly using bind
+const greet2=person.greet.bind({name:"This is bound"})
+/*instead of global object this is now explicitly bound to name: "This is bound"
+*/
+greet2()
+
+//2.setting this using call or apply
+
+person.greet.call({name:"This is using apply or call"})
